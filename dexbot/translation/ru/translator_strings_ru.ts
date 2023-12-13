1700fa032609983ff910f445d547a4579d6328bc
Dmitry <errtofordo@gmail.com>
2023-12-13 17:11:37 +0600

    Invalid format specifier fix

--- dexbot/translation/ru/translator_strings_ru.ts
+++ dexbot/translation/ru/translator_strings_ru.ts
@@ -1007,7 +1007,7 @@
     <message>
         <location filename="../../translator_strings.py" line="145"/>
         <source>Increasing {} order at price {:.8f}: {:.{prec}f} -&gt; {:.{prec}f} {} ({:.{opposite_prec}f} -&gt; {:.{opposite_prec}f} {})</source>
-        <translation>Увеличение ордера {} по цене {:.8f}: {:.{prec}f} -&gt; {:.{prec}f} {} ({:.{opposite_prec}f} -&gt; {:.{opposite_prec}f } {})</translation>
+        <translation>Увеличение ордера {} по цене {:.8f}: {:.{prec}f} -&gt; {:.{prec}f} {} ({:.{opposite_prec}f} -&gt; {:.{opposite_prec}f} {})</translation>
     </message>
     <message>
         <location filename="../../translator_strings.py" line="146"/>
